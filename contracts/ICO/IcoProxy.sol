8297151937b3464192004c2e9bce327f9ad3bf00
Łukasz Gąsior <lukas.gasior@gmail.com>
2017-08-25 12:50:00 +0200

    Add ICO proxy contract

---
+++ contracts/ICO/IcoProxy.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.13;
+
+import "../Token/Token.sol";
+
+contract IcoProxy {
+
+  address public owner;
+
+  address public icoAddress;
+
+  Token public easyMineToken;
+
+  uint256 public totalContributed;
+
+  function IcoProxy(address _icoAddress, address _easyMineTokenAddress) {
+    require(_icoAddress != 0x0);
+    require(_easyMineTokenAddress != 0x0);
+
+    owner = msg.sender;
+    icoAddress = _icoAddress;
+    easyMineToken = Token(_easyMineTokenAddress);
+  }
+
+  /* Fallback function */
+  function()
+    public
+    payable {
+    uint256 initialBalance = this.balance;
+    uint256 initialTokenBalance = easyMineToken.balanceOf(this);
+
+    assert(icoAddress.send(msg.value));
+
+    uint256 change = this.balance - initialBalance;
+    if (change != 0) {
+      assert(msg.sender.send(change));
+    }
+
+    totalContributed += msg.value - change;
+
+    uint256 newTokenBalance = easyMineToken.balanceOf(this);
+    uint256 tokensBought = newTokenBalance - initialTokenBalance;
+    assert(easyMineToken.transfer(msg.sender, tokensBought));
+  }
+
+  function withdrawAll()
+    public {
+    require(msg.sender == owner);
+
+    uint256 tokenBalance = easyMineToken.balanceOf(this);
+    assert(easyMineToken.transfer(owner, tokenBalance));
+    assert(owner.send(this.balance));
+  }
+}
